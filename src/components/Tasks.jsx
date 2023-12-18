dbadd4b9c386ac5d8630ce91ec259ce909248bd6
Manthan Srivastava <manthansrivastava99@gmail.com>
2023-12-18 21:52:15 +0530

    minor bug fix to show task related to selected projects

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,19 +1,22 @@
 import NewTask from "./NewTask";
 
-export default function Tasks({ tasks, onAdd, onDelete }) {
+export default function Tasks({ tasks, onAdd, onDelete, selectedProjectId }) {
+  const selectedProjectTasks = tasks.filter(
+    (task) => task.projectId === selectedProjectId
+  );
   return (
     <section>
       <h2 className="text-2xl font-bold text-stone-700 mb-4">Tasks</h2>
       <NewTask onAdd={onAdd} />
 
-      {tasks.length === 0 && (
+      {selectedProjectTasks.length === 0 && (
         <p className="text-stone-800 my-4">
           This project does not have any tasks yet.
         </p>
       )}
-      {tasks.length > 0 && (
+      {selectedProjectTasks.length > 0 && (
         <ul className="p-4 mt-8 rounded-md bg-stone-100">
-          {tasks.map((task) => (
+          {selectedProjectTasks.map((task) => (
             <li key={task.id} className="flex justify-between my-4">
               <span>{task.text}</span>
               <button
